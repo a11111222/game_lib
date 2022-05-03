16fe9b46110624e996fe9802be0a84da7ca049fe
Your Name <you@example.com>
2022-05-03 13:17:24 +0800

    注册系统完成 背包系统更新

--- main.py
+++ main.py
@@ -1,17 +1,17 @@
 from bag import Bag
-
+from login import login
 # 初始化背包
 try:
-    with open('bi.txt', 'r') as p:
+    with open('resources/bi.txt', 'r') as p:
         a = p.read()
-    with open('inside.txt', 'r') as p:
+    with open('resources/inside.txt', 'r') as p:
         b = p.read()
     bag = Bag(int(a), list(b))
 except FileNotFoundError:
     bag = Bag('0', '')
-    with open('bi.txt', 'w') as p:
+    with open('resources/bi.txt', 'w') as p:
         p.write(bag.bi)
-    with open('inside.txt', 'w') as p:
+    with open('resources/inside.txt', 'w') as p:
         p.write(bag.inside)
 # 克隆bag
 b = bag.inside[:]
@@ -21,5 +21,7 @@ for a in b[:]:
     if a == 'a':
         bag1.inside.remove(a)
         bag1.inside.append('生命药水')
-if __name__ == '__main__':
-    bag1.view()
+#注册
+login()
+#背包
+bag1.view()
